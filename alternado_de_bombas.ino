67493fb36d79cc837a820c40bcc859a54cf5ac07
Gildiz <44591164+Gildiz@users.noreply.github.com>
2018-10-29 20:35:37 -0300

    Add files via upload

---
+++ alternado_de_bombas.ino
@@ -0,0 +1,66 @@
+#define bomba1 6
+#define bomba2 7
+#define sensT 3
+#define sensC 4
+int Tanque = 0;
+int Cisterna = 0;
+int dato = 1; 
+
+void setup() {
+  pinMode(bomba1,OUTPUT);
+  pinMode(bomba2,OUTPUT);
+  pinMode(sensT,INPUT);
+  pinMode(sensC,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+Tanque = digitalRead(sensT);
+Cisterna = digitalRead(sensC);                            
+  
+          while(Tanque == 0){
+            Serial.println("tank0");
+          Cisterna = digitalRead(sensC);
+          Serial.println(Cisterna);
+          
+                switch(Cisterna){
+                        Serial.println("cisterna1");
+                       case 0:
+                              digitalWrite(bomba1 , LOW);
+                              digitalWrite(bomba2 , LOW);
+                              break;
+
+                       case 1:switch(dato){
+
+                              case 1: 
+                                    digitalWrite(bomba1 , HIGH);
+                                    break;
+                              case 2: 
+                                     digitalWrite(bomba2 ,HIGH);
+                                     break;
+                       
+                       }
+                }  
+                               delay(2000);
+                              Tanque = digitalRead(sensT);
+                               if (Tanque == 1){
+                                Serial.println("llegue");
+                                                switch(dato){
+                                                    
+                                                            case 1:
+                                                                    dato = dato + 1;
+                                                                    digitalWrite (bomba1, LOW);
+                                                                    break;
+                                                            case 2:
+                                                                    dato = dato - 1;
+                                                                    digitalWrite (bomba2, LOW);   
+                                                                    break;
+                           }  
+                      }       
+                
+          
+    }
+}
+
+
